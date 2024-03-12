2c8e6e8215d0564c4013077af41fb65fb1ad49cd
kent riggs <k2r3@proton.me>
2024-03-12 11:40:26 -0700

    added code to satisfy tests

--- server/app.py
+++ server/app.py
@@ -14,13 +14,46 @@ app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 
-
 @app.route('/')
 def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+@app.route('/earthquakes/<int:id>', methods=['GET'])
+def get_earthquake(id):
+    earthquake = Earthquake.query.filter_by(id=id).first()
+    if earthquake:
+        response = {
+            'id': earthquake.id,
+            'location': earthquake.location,
+            'magnitude': earthquake.magnitude,
+            'year': earthquake.year
+        }
+        return make_response(response, 200)
+    else:
+        response = {'message': f'Earthquake {id} not found.'}
+        return make_response(response, 404)
+    
+@app.route('/earthquakes/magnitude/<float:magnitude>', methods=['GET'])
+def get_earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    if earthquakes:
+        earthquakes_list = [{
+            'id': quake.id,
+            'location': quake.location,
+            'magnitude': quake.magnitude,
+            'year': quake.year
+        } for quake in earthquakes]
+        response = {
+            'count': len(earthquakes_list),
+            'quakes': earthquakes_list
+        }
+    else:
+        response = {
+            'count': 0,
+            'quakes': []
+        }
+    return make_response(response, 200)
 
 
 if __name__ == '__main__':
